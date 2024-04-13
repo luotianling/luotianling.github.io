253a7951d91f275f9baf9f03e0f228d9313f0612
洛天零 <1209408516@qq.com>
2024-04-14 00:16:57 +0800

    Site updated: 2024-04-14 00:16:57

--- js/fomal.js
+++ js/fomal.js
@@ -1113,7 +1113,7 @@ function changeMouseMode() {
 var now1 = new Date();
 
 function createtime1() {
-  var grt = new Date("30/10/2022 00:00:00"); //此处修改你的建站时间或者网站上线时间
+  var grt = new Date("10/30/2022 00:00:00"); //此处修改你的建站时间或者网站上线时间
   now1.setTime(now1.getTime() + 250);
   var days = (now1 - grt) / 1000 / 60 / 60 / 24;
   var dnum = Math.floor(days);
