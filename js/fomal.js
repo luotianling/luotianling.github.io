af1518d79aa08be0568bd68661cfd3271343967c
洛天零 <1209408516@qq.com>
2024-04-15 02:52:48 +0800

    Site updated: 2024-04-15 02:52:47

--- js/fomal.js
+++ js/fomal.js
@@ -98,7 +98,6 @@ function getDistance(e1, n1, e2, n2) {
 }
 
 function showWelcome() {
-
   let dist = getDistance(120.13026, 30.25961, ipLoacation.result.location.lng, ipLoacation.result.location.lat); //这里换成自己的经纬度
   let pos = ipLoacation.result.ad_info.nation;
   let ip;
